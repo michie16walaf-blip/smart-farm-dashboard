50a323fa9aa26ee2384b0c5af73d3981b870745a
Ambrose Jonathan Ehi <willjonathan706@gmail.com>
2026-06-13 09:47:29 +0100

    Update script.js

--- smart-farm-project/js/script.js
+++ smart-farm-project/js/script.js
@@ -20,11 +20,70 @@ item.addEventListener("click",()=>{
 const panel =
 item.querySelector(".panel");
 
-if(panel.style.display==="block"){
+if(panel.style.display==="block"){const loginForm =
+document.getElementById("loginForm");
+
+if(loginForm){
+
+loginForm.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+const workerId =
+document.getElementById("workerId").value;
+
+const password =
+document.getElementById("password").value;
+
+if(
+workerId === "SF001" &&
+password === "farm123"
+){
+
+alert("Login Successful");
+
+window.location.href =
+"index.html";
+
+}
+else{
+
+alert(
+"Invalid Worker ID or Password"
+);
+
+}
+
+});
+
+}
 panel.style.display="none";
 }
 else{
 panel.style.display="block";
+}const complaintForm =
+document.getElementById("complaintForm");
+
+if(complaintForm){
+
+complaintForm.addEventListener(
+
+"submit",
+
+function(e){
+
+e.preventDefault();
+
+alert(
+"Complaint submitted successfully."
+);
+
+complaintForm.reset();
+
+}
+
+);
+
 }
 
 });
